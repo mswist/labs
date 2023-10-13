54870fa7e1bf27ced635b1b7f86db7283dff6388
mswist <mswist@users.noreply.github.com>
2023-10-13 12:04:08 +0200

    mobile 100%

--- todo/src/script.js
+++ todo/src/script.js
@@ -1,9 +1,1 @@
-// Path: script.js
-// On load make the page 100% on mobile (hide address bar)
-window.addEventListener("load",function() {
-    // Set a timeout...
-    setTimeout(function(){
-      // Hide the address bar!
-      window.scrollTo(0, 1);
-    }, 0);
-  });
+console.log('Loaded')
